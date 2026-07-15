82c3d49ea4c2625933fc1a86347438a213fd510d
pjrpjr9003-svg <pjrpjr9003@gmail.com>
2026-07-15 15:02:31 +0900

    Add files via upload

--- sw-admin.js
+++ sw-admin.js
@@ -2,7 +2,7 @@
    Service Worker – 도시대기측정소 점검앱
    오프라인 캐싱 + localStorage 자동 백업
 ===================================================== */
-var CACHE_NAME = 'daegi-admin-v15';
+var CACHE_NAME = 'daegi-admin-v17';
 var ASSETS = [
   './',
   './user.html',
